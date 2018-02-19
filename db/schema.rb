954a4ed0f0ead7b64ed4a69b5c1742062d2156ae
laurie galeron <laurie.galeron@gmail.com>
2018-02-19 23:18:41 +0100

    modify column name quantity model order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219215617) do
+ActiveRecord::Schema.define(version: 20180219221417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20180219215617) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer "qunatity"
+    t.integer "quantity"
     t.integer "total_price"
     t.boolean "delivered"
     t.bigint "burger_id"
