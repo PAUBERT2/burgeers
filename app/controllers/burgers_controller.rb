85f754eabad1da5600546401e767ad7aaf13c118
Chloe Beauchet <chloe.beauchet@kedgebs.com>
2018-02-20 16:44:31 +0100

    burger show working

---
+++ app/controllers/burgers_controller.rb
@@ -0,0 +1,23 @@
+class BurgersController < ApplicationController
+  before_action :set_burger, only: [:show, :index]
+
+  def index
+    @burgers = Burger.all
+  end
+
+  def show
+    @burger = Burger.find(params[:id])
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+  def set_burger
+    @burger = Burger.find(params[:id])
+  end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+  def burger_params
+    params.require(:burger).permit(:name, :description, :image_url, :price, :quantity_max, :opening_days, :published)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
+  get 'burgers/index'
+
+  get 'burgers/show'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # READ
+  # Read All
+  # get "tasks", to: "tasks#index"
+  get "burgers", to: "burgers#index"
+
+  # Read one burger
+  # get "tasks/:id", to:"tasks#show", as: :task
+  get "burgers/:id", to:"burgers#show", as: :burger
+
+  # Or via resources
+  # resources :burgers, only: [:index, :show]
+  # WARNING : INDEX WHICH IS HOME
 end
