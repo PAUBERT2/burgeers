c2382be25e0b4f8000cd3e8736616d0edfec4ce7
laurie galeron <laurie.galeron@gmail.com>
2018-02-20 11:50:22 +0100

    add avatar_url to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219221417) do
+ActiveRecord::Schema.define(version: 20180220104701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20180219221417) do
     t.string "last_name"
     t.string "first_name"
     t.boolean "cooker", default: false
+    t.string "avartar_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
